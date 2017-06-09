83685a862cdead01cd4ee3551a74e6c8a76ecbfa
libin <libin2@ctsi.com.cn>
2017-06-09 09:20:08 +0800

    save

--- src/main/java/org/sevenzero/imagedemo/App.java
+++ src/main/java/org/sevenzero/imagedemo/App.java
@@ -16,6 +16,10 @@ public class App {
 			Thumbnails.of("/home/lb/temp/image/logincust-1920.jpg").size(200, 50).toFile("/home/lb/temp/image/200x50.jpg");
 			
 			Thumbnails.of("/home/lb/temp/image/logincust-1920.jpg").size(30, 30).toFile("/home/lb/temp/image/30x30.jpg");
+			
+			Thumbnails.of("/home/lb/temp/image/logincust-1920.jpg").scale(0.25F).toFile("/home/lb/temp/image/scale_0.25.jpg");
+			
+			Thumbnails.of("/home/lb/temp/image/logincust-1920.jpg").scale(0.1F).toFile("/home/lb/temp/image/scale_0.1.jpg");
 		} 
 		catch (IOException e) {
 			e.printStackTrace();
